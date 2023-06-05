35d5fc9540a9f185e434cc766b689f14077f9eeb
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-05 17:05:36 +0530

    Initial commit

---
+++ src/app/modules/modules.module.ts
@@ -0,0 +1,19 @@
+import { QuickUiModule } from 'quick-ui';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ModulesRoutingModule } from './modules-routing.module';
+import { CreateWhatIfAnalysisComponent } from './create-what-if-analysis/create-what-if-analysis.component';
+
+
+@NgModule({
+  declarations: [
+    CreateWhatIfAnalysisComponent
+  ],
+  imports: [
+    CommonModule,
+    ModulesRoutingModule
+  ,	QuickUiModule,
+]
+})
+export class ModulesModule { }
